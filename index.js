06f2fe4f75f51e44cbd9be7b8bedadd4dc71fe40
Elvis Githinji <109733200+kisinja@users.noreply.github.com>
2024-09-06 00:11:25 +0300

    commit

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
 
-const Product = require("./models/Product");
-const products = require("./data");
+/* const Product = require("./models/Product");
+const products = require("./data"); */
 
 app.listen(port, async () => {
-  await Product.insertMany(products);
+  /* await Product.insertMany(products); */
   console.log(`Backend server is running on port ${port}`);
 });
 
